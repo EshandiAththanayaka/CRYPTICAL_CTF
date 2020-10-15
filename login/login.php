c5d942a87fb6347551c4a49946167664eaf10277
EshandiAththanayaka <49785346+EshandiAththanayaka@users.noreply.github.com>
2020-10-15 17:24:06 +0530

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html >
+<html>
+<head>
+<title> LOGIN FORM</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+
+
+</head>
+<body id="body_bg">
+ <div align="center">
+
+<img src="new.jpg"  style="width:200px;height:200px;">
+<img src="new1.jpg"  style="width:200px;height:200px;">
+<img src="new2.jpg"  style="width:200px;height:200px;">
+<img src="new3.jpg"  style="width:200px;height:200px;">
+<img src="new1.jpg"  style="width:200px;height:200px;">
+<img src="new2.jpg"  style="width:200px;height:200px;">
+
+<h1 style="background-color:white;">ADMIN LOGIN FORM</h3>
+  <div>
+    <form id="login-form" method="post" action="authen_login.php" >
+        <table border="0.5" >
+            <tr>
+                <td><label for="user_id">User Name</label></td>
+                <td><input type="text" name="user_id" id="user_id"></td>
+            </tr>
+            <tr>
+                <td><label for="user_pass">Password</label></td>
+                <td><input type="password" name="user_pass" id="user_pass"></input></td>
+            </tr>
+			
+            <tr>
+				
+                <td><input type="submit" value="Submit" />
+                
+				
+            </tr>
+        </table>
+    </form>
+
+
+  </td>
+		</div>
+</body>
+</html>
